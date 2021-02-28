a68684421a815e76686eab1294cd5e6f5a574427
yashwanthmukka <yashwanthmukka15@gmail.com>
2021-02-28 22:55:39 +0530

    Maven tasks

---
+++ Maven-Afternoon/Q.3/ServletDemo/src/ServletDemo.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("")
+public class ServletDemo extends HttpServlet {
+
+	
+	 private String message;
+
+	   public void init() throws ServletException {
+	      // Do required initialization
+	      message = "Hello!! Welcome to demo of Servlets";
+	   }
+	   
+	   public void doGet(HttpServletRequest request, HttpServletResponse response)
+			      throws ServletException, IOException {
+			      
+			      // Set response content type
+			      response.setContentType("text/html");
+
+			      // Actual logic goes here.
+			      PrintWriter out = response.getWriter();
+			      out.println("<h1>" + message + "</h1>");
+	    }
+	   
+	   public void destroy() {
+		      // do nothing.
+		   }
+
+}
